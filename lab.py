c77f3b6af919af9f98d11fc5350ca8aec32746a0
Sonny <sonnybwu@mit.edu>
2022-11-30 16:07:03 -0500

    Passing 48/66 testcases

--- lab.py
+++ lab.py
@@ -189,6 +189,69 @@ def parse(tokens):
     result = parse_expression(0)[0]
     return result
 
+#Class fo defining functions
+class Function():
+    #initialize the body of function, parameters, and frame defined in
+    def __init__(self, parameters, body, frame):
+        self.parameters = parameters
+        self.body = body
+        self.frame = frame
+
+    def __call__(self, args):
+        #Check to see if enough arguments were provided
+        if len(self.parameters) != len(args):
+            raise SchemeEvaluationError
+        #make a new frame whose parent is the function's enclosing frame (this is called lexical scoping).
+        frame = Frame(self.frame)
+        #Assign each arg in args to each parameter
+        for i, parameter in enumerate(self.parameters):
+            frame.bindings[parameter] = args[i]
+        return evaluate(self.body, frame)
+
+#Class for frames
+class Frame():
+    def __init__(self, parent=None):
+        #Point to parent frame
+        self.parent = parent
+        #Create dictionary for variable bindings
+        self.bindings = {}
+
+    #We will need to be able to look up variable names in the frame
+    def lookup_var(self, var_name):
+        #If variable exists within this frames bindings, return it
+        if var_name in self.bindings:
+            return self.bindings[var_name]
+        # Check if frame has a parent if not, raise error
+        elif self.parent == None:
+            raise SchemeNameError
+        #Recursively check the next parent frame for the variable
+        else:
+            return self.parent.lookup_var(var_name)
+
+    def set_var(self, var_name,expression):
+        #If variable exists within this frames bindings, bind it to val
+        if var_name in self.bindings:
+            #Assign it
+            self.bindings[var_name] = expression
+            #When defined return the val of the exp
+            return expression
+        # Check if frame has a parent if not, raise error
+        elif self.parent == None:
+            raise SchemeNameError
+        #Recursively check the next parent frame for the variable
+        else:
+            return self.parent.lookup_var(var_name)
+
+class Pair():
+    def __init__(self, car, cdr):
+        self.car = car
+        self.cdr = cdr
+        
+    def get_car(self):
+        return self.car
+
+    def get_cdr(self):
+        return self.cdr
 
 
 ######################
@@ -211,11 +274,197 @@ def division(args):
         return result
     return func(args)
 
+def result_and_frame(tree, frame=None):
+    #Check if frame is None then this must be the global frame and its parent should be built-ins
+    if frame == None:
+        parent = Frame()
+        parent.bindings = scheme_builtins
+        frame = Frame(parent)
+    return evaluate(tree, frame), frame
+
+#should evaluate to true if all of its arguments are equal to each other.
+def equal(args):
+    if [args[0]] * len(args) == args:
+        return True
+    return False
+
+#should evaluate to true if its arguments are in decreasing order. (>)
+def greater_than(args):
+    return all(s > t for s, t in zip(args, args[1:]))
+
+#should evaluate to true if its arguments are in nonincreasing order.
+def greater_than_or_equal_to(args):
+    l = args[:]
+    l.sort(reverse=True)
+    if l == args:
+        return True
+    return False
+
+#should evaluate to true if its arguments are in increasing order.
+def less_than(args):
+    return all(s < t for s, t in zip(args, args[1:]))
+
+#should evaluate to true if its arguments are in nondecreasing order.
+def less_than_or_equal_to(args):
+    l = args[:]
+    l.sort()
+    if l == args:
+        return True
+    return False
+
+#should be a special form that takes arbitrarily many arguments and evaluates to true if all of its arguments are true.
+def _and(args):
+    for arg in args:
+        if not evaluate(arg):
+            return False
+    return True
+
+#should be a special form that takes arbitrarily many arguments and evaluates to true if any of its arguments is true.
+def _or(args):
+    for arg in args:
+        if evaluate(arg):
+            return True
+    return False
+
+#should be a built-in function that takes a single argument and should evaluate to false if its argument is true and true if its argument is false.
+def _not(arg):
+    if len(arg) == 1:
+        return not arg[0]
+    raise SchemeEvaluationError
+
+def cons(args):
+    #check for correct number of arguments
+    if len(args) != 2:
+        raise SchemeEvaluationError
+    else:
+        return Pair(*args)
+
+def car(arg):
+    if not isinstance(arg, Pair):
+        raise SchemeEvaluationError
+    return arg.get_car()
+
+def cdr(arg):
+    if not isinstance(arg, Pair):
+        raise SchemeEvaluationError
+    return arg.get_cdr()
+
+#should take an arbitrary object as input, and it should return #t if that object is a linked list, and #f otherwise.
+def is_list(args):
+    if isinstance(args, list):
+        return is_list(args[0])
+    elif isinstance(args, Pair):
+        return is_list(args.get_cdr())
+    elif args == None:
+        return True
+    return False
+
+#should take a list as argument and should return the length of that list. When called on any object that is not a linked list, it should raise a SchemeEvaluationError.
+def list_len(args):
+    # if empty list, return 0
+    if args[0] == None:
+        return 0
+    # if not a linked list
+    if not is_list([args[0]]):
+        raise SchemeEvaluationError
+    len = 1
+    #iterate through linked list until None is reached return len
+    s = args[0]
+    while s.get_cdr() != None:
+        len += 1
+        s = s.get_cdr()
+    return len
+
+def index(args):
+    """
+    Returns the item at index i given a linked list
+    """
+    # separate arguments: the linked list and index i
+    clist, i = args
+    # if linked list empty, raise an exception
+    if clist == None:
+        raise SchemeEvaluationError
+    # if a solo pair and index equal 0, return the pairs head
+    elif not is_list([clist]) and isinstance(clist, Pair):
+        if i == 0:
+            return clist.get_car()
+        # otherwise, raise exception
+        raise SchemeEvaluationError
+    # iteratively traverse linked list until we reach index i
+    curr_item = clist
+    for curr_i in range(1, i+1):
+        curr_item = curr_item.get_cdr()
+        # have reached end of linked list
+        if curr_item == None:
+            raise SchemeEvaluationError
+    # return item at index i
+    return curr_item.get_car()
+
+#create new copy of list
+def get_copy_list(args):
+    if type(args) != Pair:
+        raise SchemeEvaluationError
+    elif args.get_car() == None: 
+        return Pair(args.get_car(), args.get_cdr())
+    return Pair(args.get_car(), get_copy_list(args.get_cdr()))
+
+def append(args):
+    copy_args = []
+    for element in args:
+        if element != None:
+            copy_args.append(get_copy_list(element))
+    if len(copy_args) == 0:
+        return None
+    elif len(copy_args) == 1:
+        return Pair(copy_args[0].car(), copy_args[0].cdr())
+    current = copy_args[0]
+    car = current
+    for element in copy_args[1:]:
+        while current.get_cdr() != None:
+            current = current.get_tail()
+        current.tail = element
+    return car
+
+def make_list(args):
+    if len(args) == 0:
+        return None
+    else:
+        return Pair(args[0], make_list(args[1:]))
+
+def return_car(args):
+    if len(args) == 1 and isinstance(args[0], Pair):
+        return args[0].get_car()
+    raise SchemeEvaluationError
+
+def return_cdr(args):
+    if len(args) == 1 and isinstance(args[0], Pair):
+        return args[0].get_cdr()
+    raise SchemeEvaluationError
+    
 scheme_builtins = {
     "+": sum,
     "-": lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
     "*": multiply,
     "/": division,
+    "#t": True,
+    "#f": False,
+    "equal?": equal,
+    ">": greater_than,
+    ">=": greater_than_or_equal_to,
+    "<": less_than,
+    "<=": less_than_or_equal_to,
+    "and": _and,
+    "or": _or,
+    "not": _not,
+    "cons": cons,
+    "list?": is_list,
+    "length": list_len,
+    "list-ref": index,
+    "nil": None,
+    "append": append,
+    "list": make_list,
+    "car": return_car,
+    "cdr": return_cdr,
 }
 
 
@@ -224,7 +473,7 @@ scheme_builtins = {
 ##############
 
 
-def evaluate(tree):
+def evaluate(tree, frame=None):
     """
     Evaluate the given syntax tree according to the rules of the Scheme
     language.
@@ -233,32 +482,74 @@ def evaluate(tree):
         tree (type varies): a fully parsed expression, as the output from the
                             parse function
     """
-    # If the expression is a list (representing an S-expression), each of the elements in the 
-    # list should be evaluated, and the result of evaluating the first element (a function) 
-    # should be called with the remaining elements passed in as arguments2. The overall 
-    # result of evaluating such a function is the return value of that function call.
-    if isinstance(tree, list):
-        #Evaluate the function
-        first_element = evaluate(tree[0])
-        #If first_element is a callable function
-        if callable(first_element):
-            #Evaluate each other element
-            args = [evaluate(arg) for arg in tree[1:]]
-            #Pass the evaluated elements into function and return
-            return first_element(args)
-        #The first element must have not been a valid function
-        raise SchemeEvaluationError
-        
+    #Check if frame is None then this must be the global frame and its parent should be built-ins
+    if frame == None:
+        parent = Frame()
+        parent.bindings = scheme_builtins
+        frame = Frame(parent)
 
-    #If the expression is a symbol representing a name in scheme_builtins, 
-    #it should return the associated object.
-    elif tree in scheme_builtins:
-        #Return the object itself
-        return scheme_builtins[tree]
-    
     #If the expression is a number, it should return that number.
-    elif isinstance(tree, (int, float)):
+    if isinstance(tree, (int, float)):
         return tree
+
+    #If expression is a variable return it
+    elif isinstance(tree, str):
+        return frame.lookup_var(tree)
+
+    elif not isinstance(tree, (int, float)):
+        #Empty
+        if tree == None:
+            return None
+        elif tree == []:
+            raise SchemeEvaluationError
+        #Behavior for defining variables
+        if tree[0] == 'define':
+            #If second item is still a list its a func def
+            if isinstance(tree[1], list):
+                #Get func header (name and args)
+                header = tree[1]
+                if len(header) > 1:
+                    #Get the arguments in func header
+                    args = header[1:]
+                else:
+                    args = []
+                func = ['lambda', args, tree[2]]
+                val = evaluate(func, frame)
+                frame.bindings[tree[1][0]] = val
+                return val
+            name, expression = tree[1], tree[2]
+            val = evaluate(expression, frame)
+            frame.bindings[name] = val
+            return val
+        #Behavior for nil/empty list
+        elif tree[0] == 'nil':
+            return None
+        #Behavior for if statments
+        elif tree[0] == 'if':
+            if evaluate(tree[1], frame):
+                return evaluate(tree[2], frame)
+            else:
+                return evaluate(tree[3], frame)
+        #Behavior for defining functions with lambda
+        elif tree[0] == 'lambda':
+            parameters, body = tree[1], tree[2]
+            return Function(parameters, body, frame)
+        #Otherwise, e is a compound expression representing a function call
+        else:
+            #Evaluate the function
+            first_element = evaluate(tree[0], frame) 
+            #Case for and and or functions
+            if tree[0] == 'and' or tree[0] == 'or':
+                args = (evaluate(arg, frame) for arg in tree[1:])
+                return first_element(args)
+            #If first_element is a callable function
+            elif callable(first_element):
+                #Evaluate each other element
+                args = [evaluate(arg, frame) for arg in tree[1:]]
+                #Pass the evaluated elements into function and return
+                return first_element(args)
+            #The first element must have not been a valid function
+            raise SchemeEvaluationError
 
 
 ########
@@ -266,7 +557,42 @@ def evaluate(tree):
 ########
 
 
+# def repl(raise_all=False):
+#     while True:
+#         # read the input.  pressing ctrl+d exits, as does typing "EXIT" at the
+#         # prompt.  pressing ctrl+c moves on to the next prompt, ignoring
+#         # current input
+#         try:
+#             inp = input("in> ")
+#             if inp.strip().lower() == "exit":
+#                 print("  bye bye!")
+#                 return
+#         except EOFError:
+#             print()
+#             print("  bye bye!")
+#             return
+#         except KeyboardInterrupt:
+#             print()
+#             continue
+
+#         try:
+#             # tokenize and parse the input, then evaluate and print the result
+#             tokens = tokenize(inp)
+#             ast = parse(tokens)
+#             print("  out> ", evaluate(ast))
+#         except SchemeError as e:
+#             # if raise_all was given as True, then we want to raise the
+#             # exception so we see a full traceback.  if not, just print some
+#             # information about it and move on to the next step.
+#             #
+#             # regardless, all Python exceptions will be raised.
+#             if raise_all:
+#                 raise
+#             print(f"{e.__class__.__name__}:", *e.args)
+#         print()
+
 def repl(raise_all=False):
+    global_frame = None
     while True:
         # read the input.  pressing ctrl+d exits, as does typing "EXIT" at the
         # prompt.  pressing ctrl+c moves on to the next prompt, ignoring
@@ -285,10 +611,19 @@ def repl(raise_all=False):
             continue
 
         try:
-            # tokenize and parse the input, then evaluate and print the result
+            # tokenize and parse the input
             tokens = tokenize(inp)
             ast = parse(tokens)
-            print("  out> ", evaluate(ast))
+            # if global_frame has not been set, we want to call
+            # result_and_frame without it (which will give us our new frame).
+            # if it has been set, though, we want to provide that value
+            # explicitly.
+            args = [ast]
+            if global_frame is not None:
+                args.append(global_frame)
+            result, global_frame = result_and_frame(*args)
+            # finally, print the result
+            print("  out> ", result)
         except SchemeError as e:
             # if raise_all was given as True, then we want to raise the
             # exception so we see a full traceback.  if not, just print some
@@ -300,7 +635,6 @@ def repl(raise_all=False):
             print(f"{e.__class__.__name__}:", *e.args)
         print()
 
-
 if __name__ == "__main__":
     # code in this block will only be executed if lab.py is the main file being
     # run (not when this module is imported)
@@ -308,9 +642,6 @@ if __name__ == "__main__":
     # uncommenting the following line will run doctests from above
     # doctest.testmod()
     #repl()
-    #print(scheme_builtins['+']([1, 2, 3]))
-    print(evaluate([
-    "*",
-    7,
-    8
-  ]))
+    s = tokenize('(append (list 9 8 7))')
+    z = parse(s)
+    print(evaluate(z))
